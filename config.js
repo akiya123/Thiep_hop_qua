d147d382d6e30ff6ef12e96ac0bd7c39f5d0a231
Nguyễn Như Hoàn <92707784+akiya123@users.noreply.github.com>
2025-03-09 01:56:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Trang';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Nguyễn Thu Trang';
-const contentCard = 'Chúc cậu ngayf 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc cậu ngày 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau và tìm được câu trả lời mình cần.';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 

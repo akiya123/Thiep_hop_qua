70d4b29c1dd58687e6df412cbcbec9958fecaaa6
Nguyễn Như Hoàn <92707784+akiya123@users.noreply.github.com>
2025-03-09 09:15:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const nameGirl = 'Trang';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
+const eventName = 'Chúc Mừng 8-3';
 const titleCard = 'Nguyễn Thu Trang';
 const contentCard = 'Chúc cậu ngày 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau và tìm được câu trả lời mình cần.';
 
